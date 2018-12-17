6613009ad2451d6cb62a2234f48f0c33040c1c21
Kat Perreira <kat.perreira@gmail.com>
2018-12-16 16:38:25 -0800

    added snapshot test for card component

---
+++ src/components/test/Card.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Card from '../Card';
+import { shallow } from 'enzyme';
+
+describe('Card', () => {
+  test('that it matches an existing snapshot', () => {
+    const wrapper = shallow(
+      <Card
+      text="this is test text string"
+      emoji="test emoji"
+      />);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+
+    test('renders without crashing even with no text or emoji data', () => {
+
+      const wrapper = shallow(
+        <Card
+        text=""
+        emoji=""
+        deleteCardCallback={() => {}}
+        />);
+
+        expect(wrapper).toMatchSnapshot();
+      });
+
+
+
+
+
+    });
